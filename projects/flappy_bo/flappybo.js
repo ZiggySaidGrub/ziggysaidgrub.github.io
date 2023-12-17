c6fa60dbd105ab366d972ca2d8baef4e4af97551
ZiggySaidGrub <grubsaidgrub@gmail.com>
2023-12-17 09:52:19 -0700

    Added some projects

---
+++ projects/flappy_bo/flappybo.js
@@ -0,0 +1,30 @@
+var boboEl = document.getElementById("bo");
+var speedY = 0.1;
+var maxDown = 20;
+var maxUp = -20;
+var y = 800;
+//gravity loop
+var gravity = function() {
+	if(speedY < maxDown) {
+		speedY = speedY + 0.5;
+	}
+	if(y < 10){
+		y = 800;
+	}
+	if(y > 850){
+		y = 20;
+	}
+	y = y - speedY;
+	boboEl.style.bottom = y;
+}
+setInterval(gravity, 10);
+
+// look for spacebar press
+document.body.onkeydown = function(e) {
+  if( e.keyCode == 32 ) {
+    console.log("bobokey");
+	if(speedY > maxUp) {
+		speedY -= 30;
+	}
+  }
+}
